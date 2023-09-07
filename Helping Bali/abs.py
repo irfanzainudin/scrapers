b67b9f93f57f2e60cbe107e14ea8617bf9516263
irfanzainudin <m.irfan.zain@gmail.com>
2023-09-07 19:11:51 +1000

    got data from the initial 3 tables

--- Helping Bali/abs.py
+++ Helping Bali/abs.py
@@ -14,46 +14,43 @@ response = requests.get(url)
 # Parse the HTML content
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# table_texts = []
-# for tt in soup.find_all("table"):
-#     # table_texts.append(tt)
-#     print(tt.text)
-
-# exit(0)
-
 # Extract data from the webpage
-people = soup.table.text
+for idx, tt in enumerate(soup.find_all("table")):
+    dt = tt.text
 
-# Clean the extracted data
-people = people.split(" ")
-while '' in people:
-    people.remove('')
-people_list = []
-for i, p in enumerate(people):
-    if p[0] in CAPITALS:
-        people_list.append(p)
-    elif p[0] in LOWERCAS:
-        people_list[-1] = people_list[-1] + " " + p
-    elif p[0] in PUNCTUAS:
-        people_list[-1] = people_list[-1] + " " + p
-    elif p[0] in NUMBERS:
-        pi = p.replace(',', '')
-        people_list.append(pi)
-    else:
-        people_list.append(p)
+    # Clean the extracted data
+    dt = dt.split(" ")
+    while '' in dt:
+        dt.remove('')
+    dt_list = []
+    for i, p in enumerate(dt):
+        if p[0] in CAPITALS:
+            dt_list.append(p)
+        elif p[0] in LOWERCAS:
+            dt_list[-1] = dt_list[-1] + " " + p
+        elif p[0] in PUNCTUAS:
+            dt_list[-1] = dt_list[-1] + " " + p
+        elif p[0] in NUMBERS:
+            pi = p.replace(',', '')
+            dt_list.append(pi)
+        else:
+            dt_list.append(p)
 
-# Create a DataFrame
-# data = {'Column1': [value1], 'Column2': [value2]}
-data = {}
-col_name = ""
-for j, pl in enumerate(people_list):
-    if pl[0] not in NUMBERS:
-        col_name = pl
-        data[pl] = [0]
-    else:
-        data[col_name] = [int(pl)]
+    # Create a DataFrame
+    # data = {'Column1': [value1], 'Column2': [value2]}
+    data = {}
+    col_name = ""
+    for j, pl in enumerate(dt_list):
+        if pl[0] not in NUMBERS:
+            col_name = pl
+            data[pl] = [0]
+        else:
+            if '.' not in pl:
+                data[col_name] = [int(pl)]
+            else:
+                data[col_name] = [float(pl)]
 
-df = pd.DataFrame(data)
+    df = pd.DataFrame(data)
 
-# Export the DataFrame to an Excel file
-df.T.to_excel('output.xlsx')
+    # Export the DataFrame to an Excel file
+    df.T.to_excel(f"output{idx}.xlsx")
